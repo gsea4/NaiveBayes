ccb8c75290f353757137bc1967e1d380fd28b888
Hai H <haihuynhkbn@gmail.com>
2018-09-23 00:21:46 -0500

    Preprocess training labels

--- naive_bayes.py
+++ naive_bayes.py
@@ -7,10 +7,18 @@ training_images_file = open('train-images.idx3-ubyte','rb')
 training_images = training_images_file.read()
 training_images_file.close()
 
+training_labels_file = open('train-labels.idx1-ubyte', 'rb')
+training_labels = training_labels_file.read()
+training_labels_file.close()
+
 training_images = bytearray(training_images)
 training_images = training_images[16:]
 
+training_labels = bytearray(training_labels)
+training_labels = training_labels[8:]
+
+training_labels = np.array(training_labels)
 training_images = np.array(training_images).reshape(60000, 784)
 
-img = Image.fromarray(training_images[5].reshape((28,28)))
+img = Image.fromarray(training_images[0].reshape((28,28)))
 img.show()
