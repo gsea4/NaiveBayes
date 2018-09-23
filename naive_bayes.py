8bca355d5a3c41a0a9c4acef094107fc92c97f87
Hai <hai@localhost.localdomain>
2018-09-23 15:53:06 -0500

    Added classify function

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.misc as smp
+import math
 from PIL import Image
 
 training_images_file = open('train-images.idx3-ubyte','rb')
@@ -17,11 +18,67 @@ training_images = training_images[16:]
 training_labels = bytearray(training_labels)
 training_labels = training_labels[8:]
 
-training_labels = np.array(training_labels)
 training_images = np.array(training_images).reshape(60000, 784)
+training_labels = np.array(training_labels)
 
-img = Image.fromarray(training_images[5].reshape((28,28)))
-img.show()
 
-training_images[training_images < 30] = 0
-training_images[training_images > 30] = 1
+# img = Image.fromarray(training_images[5].reshape((28,28)))
+# img.show()
+
+training_images[training_images < 50] = 0
+training_images[training_images >= 50] = 1
+
+testing_images_file = open('t10k-images.idx3-ubyte', 'rb')
+testing_images = testing_images_file.read()
+testing_images_file.close()
+
+testing_labels_file = open('t10k-labels.idx1-ubyte', 'rb')
+testing_labels = testing_labels_file.read()
+testing_labels_file.close()
+
+testing_images = bytearray(testing_images)
+testing_images = testing_images[16:]
+
+testing_labels = bytearray(testing_labels)
+testing_labels = testing_labels[8:]
+
+testing_images = np.array(testing_images).reshape(10000, 784)
+testing_labels = np.array(testing_labels)
+
+testing_images[testing_images < 50] = 0
+testing_images[testing_images >= 50] = 1
+
+unique_elem, counts = np.unique(training_labels, return_counts = True)
+priors = np.append(unique_elem.reshape(10,1), counts.reshape(10,1), 1)
+
+likelihoods = np.zeros((10, 784), dtype='int16')
+
+for row in range(training_images.shape[0]):
+    mask = training_images[row] > 0
+    likelihoods[training_labels[row], mask] += 1
+
+def classify(image, priors, likelihoods):
+    max_pros_class = (-1E6, -1)
+    for c in range(10):
+        log_prior = math.log(priors[c][1])
+        pros = log_prior
+        n = float(np.sum(likelihoods[c]))
+        for pixel in range(28 * 28):
+            if image[pixel] == 1:
+                if likelihoods[c][pixel] == 0:
+                    likelihoods[c][pixel] = 1
+                log_likelihood = math.log(likelihoods[c][pixel]/n)
+                pros = pros + log_likelihood
+        
+        if pros > max_pros_class[0]:
+            max_pros_class = (pros, c)
+
+    return max_pros_class
+
+correct = 0
+for image in range(10000):
+    print(str(classify(testing_images[image], priors, likelihoods)[1]) + " vs " + str(testing_labels[image]))
+    if classify(testing_images[image], priors, likelihoods)[1] == testing_labels[image]:
+        correct += 1
+print(correct)
+print(str(correct/10000))
